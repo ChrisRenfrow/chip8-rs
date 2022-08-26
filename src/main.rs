0b4563bc421eb00cb12f7f2e20a75cf8b63c5841
Chris Renfrow <dev@chrisrenfrow.me>
2022-08-26 12:42:17 -0700

    Pushing non-functional code for the moment
    Co-authored-by: bmerrill42 <bmerrill42@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use std::{env, fs, io::Error};
+
+struct Config<'a> {
+    file_path: &'a str,
+}
+
 fn main() {
-    todo!()
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        eprintln!("Need file path!");
+    }
+
+    run(Config {
+        file_path: &args[1],
+    });
+}
+
+fn run(config: Config) -> Result<Vec<&str>, Error> {
+    read_file(&config.file_path)
+}
+
+fn read_file(path: &str) -> Result<Vec<&str>, Error> {
+    let file_content = fs::read_to_string(path)?;
+
+    Ok(file_content.split(' ').collect::<Vec<String>>())
 }
 
 #[cfg(test)]
@@ -10,9 +34,4 @@ mod tests {
     fn test() {
         todo!()
     }
-
-    // #[bench]
-    // fn bench() {
-
-    // }
 }
