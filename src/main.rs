82f6fc44d03cec2084dc0c0d4e8bc9da66736e78
Chris Renfrow <dev@chrisrenfrow.me>
2022-08-26 11:11:32 -0700

    Boilerplate rust project

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        todo!()
+    }
+
+    // #[bench]
+    // fn bench() {
+
+    // }
+}
